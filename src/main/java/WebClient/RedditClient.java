2078d295b1acd8dcd34cbf362b2227ed8a213f60
CrommVardek <cyrilcalier@hotmail.com>
2020-05-26 09:40:46 +0200

    dev

--- src/main/java/WebClient/RedditClient.java
+++ src/main/java/WebClient/RedditClient.java
@@ -1,12 +1,26 @@
 package WebClient;
 
-import org.springframework.stereotype.Component;
+import Domain.PrivateMessage;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+import org.springframework.web.reactive.function.client.WebClient;
+import reactor.core.publisher.Flux;
 
-@Component
+@Service
 public class RedditClient {
 
-    public RedditClient(){
+    private final WebClient webClient;
 
+    private final String REDDIT_URI;
+
+    public RedditClient(WebClient webClient, @Value("${redditUrl}") String reddit_uri){
+        this.webClient = webClient;
+        REDDIT_URI = reddit_uri;
+    }
+
+    public Flux<PrivateMessage> getUnreadMessages(){
+        webClient.get()
+                .uri
     }
 
 }
